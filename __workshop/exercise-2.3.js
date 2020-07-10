08961312b0b1a8579dc9ee79b8a9a953a6fa0630
Marie Foxall <marie@wastedeffort.ca>
2020-07-10 16:45:10 -0400

    2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -2,6 +2,23 @@
 // ----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require("request-promise");
 
 // get the code you wrote in 2.2 and paste it here...
+
+const greeting = (langCode) => {
+  return request(`https://journeyedu.herokuapp.com/hello/${langCode}`) // 1
+    .then((response) => JSON.parse(response))
+
+    .then((parsedResponse) => {
+      return parsedResponse.data.text; // 2
+    })
+    .catch((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
+};
+
+// Testing
+greeting("cr").then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
+
+greeting("ru").then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
