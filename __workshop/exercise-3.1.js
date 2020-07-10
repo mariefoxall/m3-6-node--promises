cf9e2a2622f60741a757ffce0e4c182eea2f8d2e
Marie Foxall <marie@wastedeffort.ca>
2020-07-10 16:57:35 -0400

    3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,19 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require("request-promise");
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return request("http://api.open-notify.org/iss-now.json")
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return {
+        lat: parsedResponse.iss_position.latitude,
+        lng: parsedResponse.iss_position.longitude,
+      };
+    })
+    .catch((err) => "error");
 };
+
+getIssPosition().then((result) => console.log(result));
