a6654a5e399596d823b09265d27fb4bd3a0cd6d1
Marie Foxall <marie@wastedeffort.ca>
2020-07-10 19:17:07 -0400

    3.2

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,34 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+const request = require("request");
+require("dotenv").config();
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: "f73a9022bacd492cb4a8633fcb6f4c69",
+    q: address,
   };
-
-  // return something...
+  return opencage
+    .geocode(requestObj)
+    .then((data) => {
+      // console.log(data);
+      return data.results[0].geometry;
+    })
+    .catch((error) => {
+      return "error", error.message;
+    });
 };
 
 getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
+  "1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8"
+).then((response) => console.log(response));
+
+getPositionFromAddress("London, England").then((response) =>
+  console.log(response)
+);
+
+getPositionFromAddress(
+  "435 Mclean Drive, Vancouver, BC, Canada"
 ).then((response) => console.log(response));
