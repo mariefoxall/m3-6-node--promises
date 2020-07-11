234f078c1fe7b061e7ad6da2691e90eeef26f7bb
Marie Foxall <marie@wastedeffort.ca>
2020-07-10 20:06:47 -0400

    3.3

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -1,18 +1,27 @@
 // Exercise 3.3 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: `${lat}, ${lng}`,
   };
-
-  // return ...
+  return opencage
+    .geocode(requestObj)
+    .then((data) => {
+      if (data.results.length > 0) {
+        var address = data.results[0];
+        return address.formatted;
+      }
+    })
+    .catch((error) => {
+      return "error", error.message;
+    });
 }
 
-getAddressFromPosition('48.8584', '2.2945').then((response) =>
+getAddressFromPosition("48.8584", "2.2945").then((response) =>
   console.log(response)
 );
